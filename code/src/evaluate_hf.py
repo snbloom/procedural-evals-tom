063f2b39d0801f08878be19dc96fadde2a0aeedf
Kanishk Gandhi <kanishk050296@gmail.com>
2023-07-17 16:14:45 -0700

    add local evaluation

--- code/src/evaluate_hf.py
+++ code/src/evaluate_hf.py
@@ -1,6 +1,8 @@
 import argparse
 import csv
-import os
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+
+
 
 from langchain import HuggingFaceHub
 
@@ -16,6 +18,7 @@ parser.add_argument('--max_tokens', type=int, default=100, help='max tokens')
 parser.add_argument('--num', '-n', type=int, default=1, help='number of evaluations')
 parser.add_argument('--offset', '-o', type=int, default=0, help='offset')
 parser.add_argument('--verbose', action='store_true', help='verbose')
+parser.add_argument('--local', action='store_true', help='local eval using transformers instead of huggingface hub')
 
 # data args
 parser.add_argument('--data_dir', type=str, default='../../data/conditions/three_words', help='data directory')
@@ -29,7 +32,14 @@ if args.model == '33':
     repo_id = "roneneldan/TinyStories-33M"
 elif args.model == '28':
     repo_id = "roneneldan/TinyStories-28M"
-llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature":0.0, "max_length":100})
+
+if not args.local:
+    llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature":0.0, "max_length":100})
+else:
+    model = AutoModelForCausalLM.from_pretrained(repo_id)
+    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
+
+
 
 DATA_FILE = f"{args.data_dir}/{args.init_belief}_{args.variable}_{args.condition}/stories.csv"
 CONVERTED_FILE = f"{args.data_dir}/{args.init_belief}_{args.variable}_{args.condition}/converted.txt"
@@ -53,7 +63,12 @@ for i in range(args.num):
     # story = f"{story} {prompt}"
     
     # predict answer
-    prediction = llm(converted_story)
+    if not args.local:
+        prediction = llm(converted_story)
+    else:
+        input_ids = tokenizer.encode(converted_story, return_tensors="pt")
+        output = model.generate(input_ids, max_length=100, num_beams=1)
+        prediction = tokenizer.decode(output[0], skip_special_tokens=True)
 
     # manual check for now
     print(f"Story: {story}")
