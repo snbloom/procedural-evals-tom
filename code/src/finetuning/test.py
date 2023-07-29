e63ea2a73b332a0ced3399128ed1e016415badba
Kanishk Gandhi <kanishkg@stanford.edu>
2023-07-28 21:19:14 -0700

    refine finetuning

--- code/src/finetuning/test.py
+++ code/src/finetuning/test.py
@@ -4,7 +4,7 @@ import torch
 from transformers import pipeline
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model", type=str, default="/scr/kanishkg/models/finetuned-33/checkpoint-30")
+parser.add_argument("--model", type=str, default="/scr/kanishkg/models/finetuned-28-0r/checkpoint-45")
 args = parser.parse_args()
 
 device = torch.device(0) if torch.cuda.is_available() else torch.device("cpu")
@@ -12,5 +12,8 @@ device = torch.device(0) if torch.cuda.is_available() else torch.device("cpu")
 pipe = pipeline(
     "text-generation", model=args.model, device=device
 )
-txt = 'Once upon a time, Daisy went to a fun party with her friends. They were all very hungry and wanted to eat a yummy meal. But oh no! The meal was not ready yet, it was still cold. Daisy told her friends, "The meal is cold and not ready to eat." While they were talking and having fun, there was a sound. It was the oven timer! That meant the meal was hot and ready to eat. Daisy did not hear the oven timer as she was distracted by her friends. Daisy thinks that the meal is'
-print(pipe(txt, max_new_tokens=500, num_return_sequences=1)[0]["generated_text"])
+txtfb = 'Once upon a time, Daisy went to a fun party with her friends. They were all very hungry and wanted to eat a yummy meal. But oh no! The meal was not ready yet, it was still cold. Daisy told her friends, "The meal is cold and not ready to eat." While they were talking and having fun, there was a sound. It was the oven timer! That meant the meal was hot and ready to eat. Daisy did not hear the oven timer as she was distracted by her friends. Daisy thinks that the meal is'
+txttb = 'Once upon a time, Daisy went to a fun party with her friends. They were all very hungry and wanted to eat a yummy meal. But oh no! The meal was not ready yet, it was still cold. Daisy told her friends, "The meal is cold and not ready to eat." While they were talking and having fun, there was a sound. It was the oven timer! That meant the meal was hot and ready to eat. Daisy heard the oven timer. Daisy thinks that the meal is'
+print(pipe(txttb, max_new_tokens=25, num_return_sequences=1)[0]["generated_text"])
+print(pipe(txtfb, max_new_tokens=25, num_return_sequences=1)[0]["generated_text"])
+
--- code/src/finetuning/train.py
+++ code/src/finetuning/train.py
@@ -23,7 +23,7 @@ args = parser.parse_args()
 with open(args.config, "r") as f:
     config = json.load(f)
 
-wandb.init(project="tiny-tom", dir='/scr/kanishkg/wandb/', config=config)
+wandb.init(project="tiny-tom", dir='/scr/kanishkg/wandb/', name=config["name"], config=config)
 
 # set seeds
 random.seed(config["seed"])
