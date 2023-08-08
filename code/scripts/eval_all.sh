df788ddf1d54d30543bd16b2901f04c3141fcca1
Sarah Bloom <snbloom@stanford.edu>
2023-08-08 14:36:39 -0400

    positive controls

--- code/scripts/eval_all.sh
+++ code/scripts/eval_all.sh
@@ -1,19 +1,9 @@
-# positive controls
-python ../src/auto_eval.py --model_id 28M --init_belief 0_forward --condition true_belief --num 10 --local
-python ../src/auto_eval.py --model_id 28M --init_belief 0_forward --condition false_belief --num 10 --local
-python ../src/auto_eval.py --model_id 28M --init_belief 1_forward --condition true_belief --num 10 --local
-python ../src/auto_eval.py --model_id 28M --init_belief 1_forward --condition false_belief --num 10 --local
-python ../src/auto_eval.py --model_id 33M --init_belief 0_forward --condition true_belief --num 10 --local
-python ../src/auto_eval.py --model_id 33M --init_belief 0_forward --condition false_belief --num 10 --local
-python ../src/auto_eval.py --model_id 33M --init_belief 1_forward --condition true_belief --num 10 --local
-python ../src/auto_eval.py --model_id 33M --init_belief 1_forward --condition false_belief --num 10 --local
-
-# unconverted versions
-python ../src/auto_eval.py --model_id gpt35turbo --init_belief 0_forward --condition true_belief --unconverted
-python ../src/auto_eval.py --model_id gpt35turbo --init_belief 0_forward --condition false_belief --unconverted
-python ../src/auto_eval.py --model_id gpt35turbo --init_belief 1_forward --condition true_belief --unconverted
-python ../src/auto_eval.py --model_id gpt35turbo --init_belief 1_forward --condition false_belief --unconverted
-python ../src/auto_eval.py --model_id davinci003 --init_belief 0_forward --condition true_belief --unconverted
-python ../src/auto_eval.py --model_id davinci003 --init_belief 0_forward --condition false_belief --unconverted
-python ../src/auto_eval.py --model_id davinci003 --init_belief 1_forward --condition true_belief --unconverted
-python ../src/auto_eval.py --model_id davinci003 --init_belief 1_forward --condition false_belief --unconverted
+# evaluate conditions
+python ../src/auto_eval.py --model_id 28M --init_belief 0_forward --condition true_belief --num 10 --local --unconverted
+python ../src/auto_eval.py --model_id 28M --init_belief 0_forward --condition false_belief --num 10 --local --unconverted
+python ../src/auto_eval.py --model_id 28M --init_belief 1_forward --condition true_belief --num 10 --local --unconverted
+python ../src/auto_eval.py --model_id 28M --init_belief 1_forward --condition false_belief --num 10 --local --unconverted
+python ../src/auto_eval.py --model_id 33M --init_belief 0_forward --condition true_belief --num 10 --local --unconverted
+python ../src/auto_eval.py --model_id 33M --init_belief 0_forward --condition false_belief --num 10 --local --unconverted
+python ../src/auto_eval.py --model_id 33M --init_belief 1_forward --condition true_belief --num 10 --local --unconverted
+python ../src/auto_eval.py --model_id 33M --init_belief 1_forward --condition false_belief --num 10 --local --unconverted
