14069504732f741ed5ac55aa8ca82de1f5d1658c
Sarah Bloom <snbloom@stanford.edu>
2023-08-23 15:28:36 -0500

    fix num parameter on generate_conditions_converted.py

--- code/scripts/auto_generate.sh
+++ code/scripts/auto_generate.sh
@@ -1,7 +1,6 @@
 # This script is used to generate stories in a fully automatic way.
-num_stories=95
-num_convert=200
+num_stories=100
 
 python ../src/tinytom.py --num $num_stories
-python ../src/generate_conditions_converted.py --num $num_convert
+python ../src/generate_conditions_converted.py
 python ../src/generate_conditions_unconverted.py
