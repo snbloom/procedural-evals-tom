3d329902eb7524d993b91208160d01ee3fab3054
Sarah Bloom <snbloom@stanford.edu>
2023-08-24 15:21:22 -0500

    Update auto_eval.py

--- code/src/auto_eval.py
+++ code/src/auto_eval.py
@@ -59,7 +59,7 @@ if args.model_id == "finetuned-llama-43-100": model_id = '/scr/snbloom/models/fi
 if args.model_id == "finetuned-llama-43-200": model_id = '/scr/snbloom/models/finetuned-llama-43-tinytom-200/checkpoint-65'
 if args.model_id == "finetuned-llama-43-400": model_id = '/scr/snbloom/models/finetuned-llama-43-tinytom-400/checkpoint-125'
 if args.model_id == 'finetuned-28-100': model_id = '/scr/snbloom/models/finetuned-28-tinytom-v2-100/checkpoint-80'
-if args.model_id == 'finetuned-33-100': model_id = '/scr/snbloom/models/finetuned-33-tinytom-v2-10/checkpoint-35'
+if args.model_id == 'finetuned-33-100': model_id = '/scr/snbloom/models/finetuned-33-tinytom-v2-100/checkpoint-35'
 
 data_range = f"{args.offset+1}-{args.offset + args.num}"
 
