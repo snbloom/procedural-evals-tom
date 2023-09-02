26ded354698872013f050868ad90bb1fc0b1fe6f
Sarah Bloom <snbloom@stanford.edu>
2023-09-02 00:20:47 -0700

    prep to eval on corrected in/out

--- code/scripts/eval_all.sh
+++ code/scripts/eval_all.sh
@@ -1,5 +1,10 @@
 # evaluate conditions for model in arguments
-python ../src/auto_eval.py --condition true_belief --init_belief 0_forward --model_id $1 --local --data_dir $2
-python ../src/auto_eval.py --condition false_belief --init_belief 0_forward --model_id $1 --local --data_dir $2
-python ../src/auto_eval.py --condition true_belief --init_belief 1_forward --model_id $1 --local --data_dir $2
-python ../src/auto_eval.py --condition false_belief --init_belief 1_forward --model_id $1 --local --data_dir $2
+python ../src/auto_eval.py --condition true_belief --init_belief 0_forward --model_id $1 --local --data_dir $2 --corrected_type in
+python ../src/auto_eval.py --condition false_belief --init_belief 0_forward --model_id $1 --local --data_dir $2 --corrected_type in
+python ../src/auto_eval.py --condition true_belief --init_belief 1_forward --model_id $1 --local --data_dir $2 --corrected_type in
+python ../src/auto_eval.py --condition false_belief --init_belief 1_forward --model_id $1 --local --data_dir $2 --corrected_type in
+
+python ../src/auto_eval.py --condition true_belief --init_belief 0_forward --model_id $1 --local --data_dir $2 --corrected_type out
+python ../src/auto_eval.py --condition false_belief --init_belief 0_forward --model_id $1 --local --data_dir $2 --corrected_type out
+python ../src/auto_eval.py --condition true_belief --init_belief 1_forward --model_id $1 --local --data_dir $2 --corrected_type out
+python ../src/auto_eval.py --condition false_belief --init_belief 1_forward --model_id $1 --local --data_dir $2 --corrected_type out
