6f6a44deb8b15e5bdc75e848e2f64d74a4bf2332
Sarah Bloom <snbloom@stanford.edu>
2023-09-05 19:26:17 -0700

    Update eval_all.sh

--- code/scripts/eval_all.sh
+++ code/scripts/eval_all.sh
@@ -1,3 +1,6 @@
 # evaluate conditions for model in arguments
-python ../src/auto_eval.py --condition true_belief --init_belief 0_forward --model_id finetuned-28-v3-600 --local --corrected
-python ../src/auto_eval.py --condition false_belief --init_belief 0_forward --model_id finetuned-28-v3-600 --local --corrected
+python ../src/auto_eval.py --condition true_belief --init_belief 0_backward --model_id finetuned-28-v3-100 --local --corrected
+python ../src/auto_eval.py --condition false_belief --init_belief 0_backward --model_id finetuned-28-v3-100 --local --corrected
+
+python ../src/auto_eval.py --condition true_belief --init_belief 0_backward --model_id finetuned-28-v3-600 --local --corrected
+python ../src/auto_eval.py --condition false_belief --init_belief 0_backward --model_id finetuned-28-v3-600 --local --corrected
