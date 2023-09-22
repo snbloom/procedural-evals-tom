5912bc5d0ba4776e32a04fb0615b936b620d1477
Kanishk Gandhi <kanishkg@stanford.edu>
2023-09-21 18:31:11 -0700

    change analysis

--- code/src/ts_tom_analysis.py
+++ code/src/ts_tom_analysis.py
@@ -21,10 +21,14 @@ def custom_reader(file_path):
 stories = custom_reader(data_file)
 
 # Define the regex pattern
-pattern = re.compile(r'\b(thinks|thought|believes|believed)\b\s+\w+\s+\b(is|are|were|was)\b', re.IGNORECASE)
+pattern = re.compile(r'\b(think|thinks|thought|believe|believes|believed)\b\s+\w+\s+\b(is|are|were|was)\b', re.IGNORECASE)
+# pattern = re.compile(r'\b(thinks|thought|believes|believed)\b\s+(\w+\s+)?\w+\s+\b(is|are|were|was)\b', re.IGNORECASE)
+
 
 num_tom_stories = 0
 num_tom_sentences = 0
+sentences = []
+stories_tom = []
 
 # Iterate over each line (story) in the file and find sentences that match the pattern
 for story in tqdm(stories):
@@ -34,8 +38,15 @@ for story in tqdm(stories):
         if matches:
             tom_detected = True
             num_tom_sentences += 1
+            sentences.append(sentence.replace('\n',' ')+'\n')
     if tom_detected:
         num_tom_stories += 1
+        stories_tom.append(story.replace('\n',' ')+'\n')
 
 print("Number of stories with Tom: ", num_tom_stories)
 print("Number of sentences with Tom: ", num_tom_sentences)
+
+with open('/scr/kanishkg/TinyStories/ts-tom.txt','w') as f:
+    f.writelines(stories_tom[:20])
+with open('/scr/kanishkg/TinyStories/ts-tom-sentences.txt','w') as f:
+    f.writelines(sentences[:100])
