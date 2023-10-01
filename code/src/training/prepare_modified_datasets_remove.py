6a0453e74cb0fc31fc25bd389667a9f3352e42bd
Kanishk Gandhi <kanishk050296@gmail.com>
2023-10-01 15:29:49 -0700

    refine modification script

--- code/src/training/prepare_modified_datasets_remove.py
+++ code/src/training/prepare_modified_datasets_remove.py
@@ -3,11 +3,13 @@ import json
 from tqdm import tqdm
 import argparse
 
+from prepare_gpt4 import custom_reader
+
 # load data from hf datasets
 TS_DIR = "/scr/kanishkg/TinyStories/"
 TS_DIR_OUT = "/scr/kanishkg/TinyStories/"
-TINYSTORIES_V2 = "/scr/kanishkg/TinyStories/TinyStoriesV2-GPT4-train.txt"
-TINYSTORIES_V2_VAL = "/scr/kanishkg/TinyStories/TinyStoriesV2-GPT4-valid.txt"
+TS_V2 = "/scr/kanishkg/TinyStories/TinyStoriesV2-GPT4-train.txt"
+TS_V2_VAL = "/scr/kanishkg/TinyStories/TinyStoriesV2-GPT4-valid.txt"
 train_file = os.path.join(TS_DIR, "TinyStories-train.txt")
 val_file = os.path.join(TS_DIR, "TinyStories-valid.txt")
 
@@ -45,79 +47,47 @@ def has_no_banned_words(text):
         if word.lower() in text.lower(): return False
     return True
 
-def has_banned_words(text):
-    for word in banned_words:
-        if word in text: return True
-    return False
+def filter_and_replace(ts, ts_v2):
 
-def get_tiny_stories_v2(train=True):
-    stories = []
-    most_recent = ""
-    if train: path = TINYSTORIES_V2
-    else: path = TINYSTORIES_V2_VAL
+    dataset = []
+    num_replaced = 0
+    replacement_idx = 0
 
-    with open(os.path.join(path), 'r') as f:
-        for line in tqdm(f):
-            if line.strip() != "<|endoftext|>": most_recent += line.strip().replace('\n', "")
-            else:
-                most_recent = most_recent.replace('\n', "")
-                stories.append(most_recent)
-                most_recent = ""
-    print(f"Number of tinystories_v2 stories: {len(stories)}")
-    return stories
+    for story in tqdm(ts):
+        # append the story to the dataset if no banned words
+        if has_no_banned_words(story["text"]):
+            dataset.append(story)
+        # if has banned words, replace with a story from the other dataset (with no banned words)
+        else: 
+            num_replaced += 1
+            replacement = ts_v2[replacement_idx] 
+            while not has_no_banned_words(replacement["text"]):
+                replacement_idx += 1
+                if replacement_idx == len(ts_v2)-1: print("ERROR: replacement_idx at length of v2 stories")
+                replacement = ts_v2[replacement_idx] 
+            replacement_idx += 1
+            dataset.append(replacement)
 
-def custom_reader(file_path, tinystories_v2):
+    print(f"Number tinystories swapped: {num_replaced}")
+    print(f"Number of v2 tinystories tried: {replacement_idx}")
+    print(f"Number of v2 tinystories with banned words: {replacement_idx - num_replaced}")
+    return dataset
 
-    # Open the file at the specified path
-    with open(file_path, 'r', encoding='utf-8') as file:
-        
-        dataset = []
-        num_replaced = 0
-        replacement_idx = 0
-        most_recent = ""
 
-        for line in tqdm(file):
-            # if not end of text label, add this line to the most recent story
-            if line.strip() != "<|endoftext|>": most_recent += line.strip().replace('\n', "")
-            # otherwise, this is the end of the story
-            else:
-                # append the story to the dataset if no banned words
-                if has_no_banned_words(most_recent): dataset.append({"text": most_recent})
-                # if has banned words, replace with a story from the other dataset (with no banned words)
-                else: 
-                    num_replaced += 1
-                    replacement = tinystories_v2[replacement_idx] 
-                    while has_banned_words(replacement):
-                        replacement_idx += 1
-                        if replacement_idx == len(tinystories_v2)-1: print("ERROR: replacement_idx at length of v2 stories")
-                        replacement = tinystories_v2[replacement_idx] 
-                    replacement_idx += 1
-                    dataset.append({"text": replacement})
-                # clear string tracker for next story
-                most_recent = ""
-        print(f"Number tinystories swapped: {num_replaced}")
-        print(f"Number of v2 tinystories tried: {replacement_idx}")
-        print(f"Number of v2 tinystories with banned words: {replacement_idx - num_replaced}")
-        return dataset
+train_ex = custom_reader(train_file)
+print(f"v1 train stories: {len(train_ex)}")
+val_ex = custom_reader(val_file)
+print(f"v1 val stories: {len(val_ex)}")
 
-print()
-print(f"TRAIN AND VAL FILTERING FOR CONDITION: {args.banned_words.upper()}")
-print("Getting stories v2")
-tinystories_v2 = get_tiny_stories_v2()
-print()
-print("Getting stories v2 valid")
-tinystories_v2_val = get_tiny_stories_v2(train=False)
-print()
+v2_train = custom_reader(TS_V2)
+print(f"v2 train stories: {len(v2_train)}")
+v2_val = custom_reader(TS_V2_VAL)
+print(f"v2 val stories: {len(v2_val)}")
 
-print("Reading in train file")
-train_ex = custom_reader(train_file, tinystories_v2)
-print(f"Train length: {len(train_ex)}")
-print()
-
-print("Reading in val file")
-val_ex = custom_reader(val_file, tinystories_v2_val)
-print(f"Val length: {len(val_ex)}")
-print()
+print(f"Filtering and replacing banned words for train: {banned_words}")
+new_train = filter_and_replace(train_ex, v2_train)
+print(f"Filtering and replacing banned words for val: {banned_words}")
+new_val = filter_and_replace(val_ex, v2_val)
 
 def store_json(path, data_dict):
     with open(path, 'w', encoding='utf-8') as f:
